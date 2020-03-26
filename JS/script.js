afe59a33ea4c86761095087f8d58ad4349ed33c4
Claudio Rivera <claudiorivera@me.com>
2020-03-26 15:29:02 -0500

    Optimized JS logic to be reusable and cleaner

--- JS/script.js
+++ JS/script.js
@@ -4,43 +4,35 @@ const CALCULATE = document.querySelector("button[name='calculateTotalAmount']");
 const DISPLAY = document.querySelector(".displayArea");
 const TOTAL = document.querySelector(".totalAmountResult");
 
-// Variables - Converted to Ints and Floats
-let tipRate = parseInt(TIP.value);
-let subTotal = parseFloat(SUBTOTAL.value);
-
-// Function that returns the result of multiplying
-// a sub total and a tax rate
-function calculateTipAmount() {
+// Function that returns the tip amount,
+// given a subTotal and tipRate, to the
+// nearest cent
+var calculateTipAmount = (subTotal, tipRate) => {
   return Math.round(subTotal * tipRate) / 100;
-}
+};
 
-// Function that stores the result of adding
-// the sub total and the tax amount, in dollars and cents
-// to 2 decimal places
-function calculateTotalAmount() {
-  let tipAmount = calculateTipAmount();
+// Function that returns the total amount,
+// given a subTotal and tipAmount
+var calculateTotalAmount = (subTotal, tipAmount) => {
   return subTotal + tipAmount;
-}
+};
 
-// Displays the total
+// Event handler function that displays the total
+// in the textContent div
 function displayTotal() {
-  // Unhide the results display...
-  DISPLAY.style.display = "block";
-  // ...and update the amount
-  TOTAL.textContent = `$${calculateTotalAmount().toFixed(2)}`;
-}
+  // Get the current form values...
+  let subTotal = parseFloat(SUBTOTAL.value);
+  let tipRate = parseInt(TIP.value);
 
-// Event handlers
+  // ...calculate the totalAmount...
+  TOTAL.textContent = `$${calculateTotalAmount(
+    subTotal,
+    calculateTipAmount(subTotal, tipRate)
+  ).toFixed(2)}`;
+
+  // ...and unhide the results display...
+  DISPLAY.style.display = "block";
+}
 
 // When the Calculate button is clicked, call the displayTotal function
 CALCULATE.onclick = displayTotal;
-
-// When the Tip Amount is changed, update tipRate
-TIP.addEventListener("change", event => {
-  tipRate = parseInt(TIP.value);
-});
-
-// When the Sub Total is changed, update subTotal
-SUBTOTAL.addEventListener("input", event => {
-  subTotal = parseFloat(SUBTOTAL.value);
-});
